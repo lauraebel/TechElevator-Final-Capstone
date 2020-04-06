8dfe58f8c6844265947cd5120b719d6e5fa97deb
Katherine Lillemoen <katherine.lillemoen@gmail.com>
2020-04-06 15:02:19 -0400

    removed mvc folder

--- DB/tool-lending-library.sql
+++ DB/tool-lending-library.sql
@@ -7,22 +7,22 @@ DROP TABLE IF EXISTS tool;
 DROP TABLE IF EXISTS brand;
 
 CREATE TABLE role (
-role_id serial primary key,
+id serial primary key,
 role_name varchar(35) NOT NULL
 );
 
 CREATE TABLE category (
-category_id serial primary key,
+id serial primary key,
 category_name varchar(35) NOT NULL
 );
 
 CREATE TABLE brand (
-brand_id serial primary key,
+id serial primary key,
 brand_name varchar(35) NOT NULL
 );
 
 CREATE TABLE tool (
-tool_id serial primary key,
+id serial primary key,
 tool_name varchar(60) NOT NULL,
 tool_description varchar(255),
 brand_id int,
@@ -31,7 +31,7 @@ constraint fk_brand_id foreign key (brand_id) references brand(brand_id)
 );
 
 CREATE TABLE system_user (
-system_user_id serial primary key,
+id serial primary key,
 first_name varchar(35) NOT NULL,
 last_name varchar(35) NOT NULL,
 email varchar(100) NOT NULL,
@@ -39,18 +39,18 @@ user_password varchar (100) NOT NULL,
 drivers_license varchar(35) NOT NULL,
 role_id int,
 
-constraint fk_role_id foreign key (role_id) references role(role_id)
+constraint fk_role_id foreign key (role_id) references role(id)
 );
 
 CREATE TABLE reservation_log (
-reservation_id serial primary key,
-system_user_id int,
+id serial primary key,
+user_id int,
 tool_id int,
 date_loaned date,
 date_due date,
 date_returned date,
 
-constraint fk_system_user_id foreign key (system_user_id) references system_user(system_user_id),
+constraint fk_system_user_id foreign key (system_user_id) references system_user(id),
 constraint fk_tool_id foreign key (tool_id) references tool(tool_id)
 );
 
@@ -58,7 +58,7 @@ CREATE TABLE tool_category (
 tool_id int,
 category_id int,
 
-constraint fk_tool_tool_id foreign key (tool_id) references tool(tool_id),
-constraint fk_category_id foreign key (category_id) references category(category_id),
+constraint fk_tool_tool_id foreign key (tool_id) references tool(id),
+constraint fk_category_id foreign key (category_id) references category(id),
 PRIMARY KEY(tool_id, category_id)
 );
