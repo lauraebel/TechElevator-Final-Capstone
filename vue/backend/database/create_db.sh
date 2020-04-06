2341cf4f783f89c92c1692f1b4a2c5bfc51c2aa5
Katherine Lillemoen <katherine.lillemoen@gmail.com>
2020-04-06 17:55:00 -0400

    Database files completed

--- vue/backend/database/create_db.sh
+++ vue/backend/database/create_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
-DATABASE=userdb
+DATABASE=tool_library
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
