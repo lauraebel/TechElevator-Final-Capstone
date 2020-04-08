760901005af73c3ae7090363d6692d22fa1333d1
unknown <marthaweisheimer@gmail.com>
2020-04-08 11:04:33 -0400

    Added Brand Object and updated Tool files

---
+++ vue/backend/src/main/java/com/techelevator/model/Brand.java
@@ -0,0 +1,24 @@
+package com.techelevator.model;
+
+public class Brand {
+	
+	private long brandId;
+	private String brandName;
+	
+	public long getBrandId() {
+		return brandId;
+	}
+	
+	public void setBrandId(long brandId) {
+		this.brandId = brandId;
+	}
+	
+	public String getBrandName() {
+		return brandName;
+	}
+	
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+	
+}
