00b2fd849584332db78361a342bab10b49d320e4
unknown <marthaweisheimer@gmail.com>
2020-04-09 14:15:09 -0400

    AddToCart component created

--- vue/frontend/src/App.vue
+++ vue/frontend/src/App.vue
@@ -4,6 +4,7 @@
     <cart-icon />
     <menu-icon v-on:clickedMenu="handleMenu" />
     <dropdown-menu v-bind:isVisible="visible"/>
+    <add-to-cart v-on:clickedCart="addToCart"/>
     <router-view class="content"/>
     <router-link :to="{name: 'credits'}" id="footer" >Credits</router-link>
   </div>
@@ -14,6 +15,7 @@ import MenuIcon from './components/MenuIcon'
 import DropdownMenu from './components/DropdownMenu'
 import Branding from './components/Branding'
 import CartIcon from './components/CartIcon'
+import AddToCart from './components/AddToCart'
 
 export default {
   name: 'app',
@@ -21,16 +23,21 @@ export default {
     Branding,
     CartIcon,
     DropdownMenu,
-    MenuIcon
+    MenuIcon,
+    AddToCart
   },
   data (){
     return {
-      visible: false
+      visible: false,
+      addedToCart: false
     }
   },
   methods: {
     handleMenu() {
       this.visible = !this.visible;
+    },
+    AddToCart() {
+      this.addedToCart = !this.addedToCart;
     }
   }
 }
---
+++ vue/frontend/src/components/AddToCart.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="add-to-cart">
+    <transition name="fade">
+      <div v-if="isAvailable">
+        <button v-on:click="clickedCart"><img src="@/assets/images/icons/add-to-cart.png" class="add-to-cart-icon" /></button>
+      </div>
+      <div v-if="isNotAvailable">
+        <button :disabled='isDisabled'><img src="@/assets/images/icons/add-to-cart.png" class="can-not-add-to-cart-icon" /></button> 
+      </div>
+      <div v-if="addedToCart">
+        <button v-on:click="clickedCart"><img src="@/assets/images/icons/in-cart.png" class="in-cart-icon" /></button>
+      </div>
+    </transition> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: "add-to-cart",
+  props: {
+    isAvailable: Boolean
+  },
+  data (){
+    return {
+        addedToCart: false
+    }
+  }, 
+  methods: {
+    clickedCart() {
+      this.addedToCart = !this.addedToCart;
+      this.$emit('clickedCart', this.addedToCart);
+    }
+  }
+};
+</script>
