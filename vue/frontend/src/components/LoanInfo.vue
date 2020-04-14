5e85c621e842004f75d9f74ff44b1584e0c48281
Laura Ebel <laura.ebel7@gmail.com>
2020-04-14 13:47:29 -0400

    Still working on Loan CSS

--- vue/frontend/src/components/LoanInfo.vue
+++ vue/frontend/src/components/LoanInfo.vue
@@ -2,7 +2,7 @@
   <div class="loan-info">
     <h1 class="tool-name">{{loan.tool.toolName}}</h1>
     <img v-bind:src="require(`@/assets/images/product-img/${loan.tool.toolImgName}`)" class="tool-img"/>
-    <p class="loaned-on">Loaned On: {{loan.dateLoaned.month}} {{loan.dateLoaned.dayOfMonth}}, {{loan.dateLoaned.year}}</p>
+    <p class="loaned-on">Loaned On: {{getDate(loan.dateLoaned.month)}} {{loan.dateLoaned.dayOfMonth}}, {{loan.dateLoaned.year}}</p>
     <p class="due-on">Due: {{loan.dueDate.month}} {{loan.dueDate.dayOfMonth}}, {{loan.dueDate.year}}</p>
   </div>
 </template>
@@ -12,6 +12,15 @@ export default {
   name: "loan-info",
   props: {
     loan: Object
+  },
+  methods: {
+    getDate(javaDate) {
+     const day = javaDate.dayOfMonth;
+     const month = javaDate.monthValue-1;
+     const year = javaDate.year;
+     const date = new Date(Date.UTC(year, month, day));
+     return date;
+    }
   }
 }
 </script>
