64bb679ea0eaef75e24705aee6244350a83731db
Katherine Lillemoen <katherine.lillemoen@gmail.com>
2020-04-16 14:17:32 -0400

    Fixed loan tiles

--- vue/frontend/src/components/LoanInfo.vue
+++ vue/frontend/src/components/LoanInfo.vue
@@ -2,8 +2,10 @@
   <div class="loan-info">
     <h1 class="tool-name">{{loan.tool.toolName}}</h1>
     <img v-bind:src="require(`@/assets/images/product-img/${loan.tool.toolImgName}`)" class="tool-img"/>
-    <p class="dates">Loaned On: {{getDate(loan.dateLoaned)}}<br />Due: {{getDate(loan.dueDate)}}</p>
-    <return-tool v-bind:loan="loan" />
+    <div class="more-info">
+      <p class="dates">Loaned: {{getDate(loan.dateLoaned)}}<br />Due: {{getDate(loan.dueDate)}}</p>
+      <return-tool v-bind:loan="loan" />
+    </div>
   </div>
 </template>
 
