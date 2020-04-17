4d4011ffec04eca673b7e8bd55bfbe3468d4c170
Laura Ebel <laura.ebel7@gmail.com>
2020-04-16 21:08:54 -0400

    Do not use

--- vue/frontend/src/components/ToolDetails.vue
+++ vue/frontend/src/components/ToolDetails.vue
@@ -1,13 +1,49 @@
 <template>
-  
+  <div class="tool-info">
+    <h1 class="tool-name">{{ tool.toolName }}</h1>
+    <img
+      v-bind:src="
+        require(`@/assets/images/product-img/${tool.toolImgName}`)
+      "
+      class="tool-img"
+    />
+    <p class="description">
+       {{ tool.toolDescription }}
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+    name: "tool-details",
+    props: {
+    tool: Object,
+  },
+  data() {
+    return {
+      tool: {}
+    };
+  },
+methods: {
+    getTool() {
+      fetch(
+        `${process.env.VUE_APP_REMOTE_API}/api/tools/${this.tool.toolId}`,
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json(); 
+          }
+        })
+        .then((data) => {
+          this.tool = data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+  created() {
+    this.getTool();
+  },
+};
 </script>
-
-<style>
-
-</style>
--- vue/frontend/src/views/Tool.vue
+++ vue/frontend/src/views/Tool.vue
@@ -1,14 +1,50 @@
 <template>
-  <div class="tool-detail">
-    <h1>Tool Detail</h1>
-    <p>You must be authenticated to see this</p>
+  <div class="tool-details">
+    <h1 class="page-title">Tool Details</h1>
+    <reservation-info
+      v-for="tool in tools"
+      v-bind:key="tool.id"
+      v-bind:tool="tool"
+    />
   </div>
 </template>
 
 <script>
+import ToolDetails from "../components/ToolDetails";
+
 export default {
-  name: 'tool-detail'
-}
+  name: 'tool-view',
+    components: {
+    ToolDetails,
+  },
+    data() {
+    return {
+      tool: "",
+    };
+  },
+  methods: {
+    getReservations() {
+      fetch(
+        `${process.env.VUE_APP_REMOTE_API}/api/tools/${
+          auth.getUser().sub
+        }`,
+        {
+          headers: {
+            Authorization: `Bearer ${auth.getToken()}`,
+          },
+        }
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.allTools = data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+  }
+  }
 </script>
 
 <style>
